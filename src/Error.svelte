d2eb53fc1fce11cf73465454d6f04166751b2c6f
Chris Weiskopf <chris@chriscancode.io>
2023-02-28 12:59:30 -0500

    adding styling to errors

--- src/Error.svelte
+++ src/Error.svelte
@@ -4,10 +4,24 @@
 
 {#if errors}
   {#each errors as error}
-  <div>
+  <div class="error-container">
     <ul>
       <li class="error">{error.msg}</li>
     </ul>
   </div>
   {/each}
 {/if}
+
+<style>
+
+  .error-container{
+    border-radius: 1px solid rgb(110, 23, 23);
+    background-color: rgba(233, 93, 93, 0.3);
+  }
+
+  ul{
+    list-style: none;
+  }
+
+  
+</style>
