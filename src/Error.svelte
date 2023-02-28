ff69cde7436e609869c7b8c1747aba77a0e292d2
Chris Weiskopf <chris@chriscancode.io>
2023-02-28 13:09:36 -0500

    tweaking error styles

--- src/Error.svelte
+++ src/Error.svelte
@@ -3,20 +3,22 @@
 </script>
 
 {#if errors}
+<div class="error-container">
+  <ul>
   {#each errors as error}
-  <div class="error-container">
-    <ul>
       <li class="error">{error.msg}</li>
-    </ul>
-  </div>
   {/each}
+</ul>
+</div>
 {/if}
 
 <style>
 
   .error-container{
-    border-radius: 1px solid rgb(110, 23, 23);
-    background-color: rgba(233, 93, 93, 0.3);
+	border: 1px solid rgb(110, 23, 23);
+	background-color: rgba(233, 93, 93, 0.3);
+	width: 30%;
+	margin: 0 auto;
   }
 
   ul{
