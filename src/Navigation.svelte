1c79146914144287e122d61244e117be3fca7963
Chris Weiskopf <chris@chriscancode.io>
2023-03-05 10:09:42 -0500

    Reworking CSS in navigation, and making it more DRY

--- src/Navigation.svelte
+++ src/Navigation.svelte
@@ -7,7 +7,33 @@
   let toggleModal
 </script>
 
-{#if $userStore}
+<div class="control-panel">
+  <h1>LotTJ Fates</h1>
+    <div>
+    {#if $userStore}
+    <h2 class="capitalize">Welcome, {$userStore.user.username}</h2>
+    {:else}
+    <Login />
+    {/if}
+    <nav use:links>
+      <Router>
+        <a href="/">Home</a>
+        {#if $userStore}
+        <a
+        href="https://chriscancodecms.up.railway.app/"
+        target="_blank"
+        rel="noreferrer">Go to Dashboard</a>
+      {/if}
+        <p on:click={toggleModal}>Toggle Roll Table</p>
+      </Router>
+    </nav>
+</div>
+
+</div>
+
+<RollTable bind:toggleModal={toggleModal}/>
+
+<!-- {#if $userStore}
   <div class="control-panel">
     <h1 class="capitalize">Welcome, {$userStore.user.username}</h1>
     <a
@@ -33,16 +59,54 @@
     <button on:click={toggleModal}>Toggle Roll Table</button>
   </div>
 {/if}
-<RollTable bind:toggleModal={toggleModal}/>
+ -->
 
 <style>
-  .control-panel {
-    top: 0;
-    position: sticky;
-    width: 100%;
-    text-align: center;
-    background-color: var(--accent);
-    padding: 0;
-    border-bottom: 3px solid var(--darker-hlight);
-  }
+.control-panel {
+	top: 0;
+  display: flex;
+  justify-content: space-between;
+	position: sticky;
+	width: 100%;
+	text-align: center;
+	background-color: var(--accent);
+	padding: 0;
+	border-bottom: 3px solid var(--darker-hlight);
+}
+
+.control-panel > div {
+	display: flex;
+	flex-direction: column;
+	max-width: 350px;
+	flex-grow: 2;
+}
+
+.control-panel > div > nav {
+  display: flex;
+  justify-content: space-around;
+}
+
+ a {
+  color: black;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+
+.control-panel > div > nav > a:hover{
+  text-decoration: underline;
+}
+
+ p {
+  margin: 0;
+  cursor: pointer;
+}
+
+ p:hover{
+  text-decoration: underline;
+}
+
+a{
+  color: inherit;
+  text-decoration: none;
+}
 </style>
